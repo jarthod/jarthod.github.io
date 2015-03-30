4ad88a9676247ba2f4fee10adc9d2c6b152c97b0
Adrien Jarthon <jobs@adrienjarthon.com>
2015-03-30 21:55:48 +0200

    switch to middleman

---
+++ config.rb
@@ -0,0 +1,19 @@
+compass_config do |config|
+  config.output_style = :compressed
+end
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
+
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+
+# Build-specific configuration
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :relative_assets
+end
